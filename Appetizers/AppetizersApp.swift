329f084968b840d6b0392bde386282ed843a8f37
Gabriel Henrique <gabhenrique0@gmail.com>
2024-01-17 17:29:49 -0300

    Config: create project initial structure

---
+++ Appetizers/AppetizersApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppetizersApp.swift
+//  Appetizers
+//
+//  Created by Gabriel Pereira on 17/01/24.
+//
+
+import SwiftUI
+
+@main
+struct AppetizersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
