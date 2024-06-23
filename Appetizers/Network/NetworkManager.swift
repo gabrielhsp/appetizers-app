36f0b4286f6834d0ef8cfeb4e76329054bf64292
Gabriel Henrique <gabhenrique0@gmail.com>
2024-06-23 18:07:54 -0300

    Feature: add a method on network manager to download image remotely

--- Appetizers/Network/NetworkManager.swift
+++ Appetizers/Network/NetworkManager.swift
@@ -5,13 +5,14 @@
 //  Created by Gabriel Pereira on 18/02/24.
 //
 
-import Foundation
+import UIKit
 
 final class NetworkManager {
     // MARK: - Properties
+    private let cache = NSCache<NSString, UIImage>()
     static let shared = NetworkManager()
     static let baseURL = "https://seanallen-course-backend.herokuapp.com/swiftui-fundamentals"
-    private let appetizersURL = baseURL + "/appetizers"
+    private let appetizersURL = baseURL + "/appetixzers"
     
     // MARK: - Initializer Methods
     private init() {}
@@ -47,6 +48,34 @@ final class NetworkManager {
             } catch {
                 completed(.failure(.invalidData))
             }
+        }
+        
+        task.resume()
+    }
+    
+    func downloadImage(fromURLString urlString: String, completed: @escaping (UIImage?) -> Void) {
+        let cacheKey: NSString = NSString(string: urlString)
+        
+        if let image = cache.object(forKey: cacheKey) {
+            completed(image)
+            return
+        }
+        
+        guard let url = URL(string: urlString) else {
+            completed(nil)
+            return
+        }
+        
+        let task = URLSession.shared.dataTask(with: URLRequest(url: url)) { data, response, error in
+            guard let data = data,
+                  let image = UIImage(data: data) else {
+                completed(nil)
+                return
+            }
+            
+            self.cache.setObject(image, forKey: cacheKey)
+            
+            completed(image)
         }
         
         task.resume()
