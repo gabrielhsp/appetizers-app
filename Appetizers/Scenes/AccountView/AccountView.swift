b608ac8a2156603aa877c924f88312789b2e06d7
Gabriel Henrique <gabhenrique0@gmail.com>
2024-07-10 22:45:28 -0300

    Feature: create the structure of AccountView

--- Appetizers/Scenes/AccountView/AccountView.swift
+++ Appetizers/Scenes/AccountView/AccountView.swift
@@ -8,10 +8,40 @@
 import SwiftUI
 
 struct AccountView: View {
+    @State private var firstName: String = ""
+    @State private var lastName: String = ""
+    @State private var email: String = ""
+    @State private var birthdate: Date = Date()
+    @State private var extraNapkins: Bool = false
+    @State private var frequentRefills: Bool = false
+    
     var body: some View {
         NavigationView {
-            Text("Account")
-                .navigationTitle("🤣 Account")
+            Form {
+                Section(header: Text("Personal Info")) {
+                    TextField("First Name", text: $firstName)
+                    TextField("Last Name", text: $lastName)
+                    TextField("Email", text: $email)
+                        .keyboardType(.emailAddress)
+                        .textInputAutocapitalization(.never)
+                        .autocorrectionDisabled()
+                    
+                    DatePicker("Birthday", selection: $birthdate, displayedComponents: .date)
+                    
+                    Button {
+                        print("Save")
+                    } label: {
+                        Text("Save Changes")
+                    }
+                }
+                
+                Section(header: Text("Requests")) {
+                    Toggle("Extra Napkins", isOn: $extraNapkins)
+                    Toggle("Frequent Refills", isOn: $frequentRefills)
+                }
+                .toggleStyle(SwitchToggleStyle(tint: .brandPrimary))
+            }
+            .navigationTitle("🤣 Account")
         }
     }
 }
