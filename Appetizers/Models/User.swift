adee5bd0a10f180c27bd3a030d93687e3a0ee824
Gabriel Henrique <gabhenrique0@gmail.com>
2024-07-12 19:29:57 -0300

    Feature: move the view model properties to user model

--- Appetizers/Models/User.swift
+++ Appetizers/Models/User.swift
@@ -6,3 +6,12 @@
 //
 
 import Foundation
+
+struct User: Codable {
+    var firstName: String = ""
+    var lastName: String = ""
+    var email: String = ""
+    var birthdate: Date = Date()
+    var extraNapkins: Bool = false
+    var frequentRefills: Bool = false
+}
