a3ef7a03c09d08c03367b9c7c602baf934033b39
Gabriel Henrique <gabhenrique0@gmail.com>
2024-07-15 21:30:01 -0300

    Feature: create the structure of order view

--- Appetizers/Scenes/OrderView/OrderView.swift
+++ Appetizers/Scenes/OrderView/OrderView.swift
@@ -8,11 +8,34 @@
 import SwiftUI
 
 struct OrderView: View {
+    @State private var orderItems: [Appetizer] = MockData.orderItems
+    
     var body: some View {
         NavigationView {
-            Text("Orders")
-                .navigationTitle("🧾 Orders")
+            VStack {
+                List {
+                    /// We're using a `ForEach` here to be able to use the delete swipe action
+                    /// It's not available on a `List` element
+                    ForEach(orderItems) { appetizer in
+                        AppetizerListCell(appetizer: appetizer)
+                    }
+                    .onDelete(perform: deleteItems)
+                }
+                .listStyle(.plain)
+                
+                Button {
+                    print("Order placed")
+                } label: {
+                    APButton(title: "$99.99 - Place Order")
+                }
+                .padding(.bottom, 25)
+            }
+            .navigationTitle("🧾 Orders")
         }
+    }
+    
+    func deleteItems(at offsets: IndexSet) {
+        orderItems.remove(atOffsets: offsets)
     }
 }
 
